a4340481e5c3f4282a22651352263e9b488a7f4c
PierreLgol <plgol.perso@gmail.com>
2025-01-26 08:12:59 +0100

    Reworked project file structure and build system

--- pong/server/build.zig
+++ pong/server/build.zig
@@ -4,20 +4,46 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
+    const lib_mod = b.createModule(.{
+        .root_source_file = b.path("src/libpong/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const libpong = b.addStaticLibrary(.{
+        .name = "libpong",
+        .root_module = lib_mod,
+    });
+    b.installArtifact(libpong);
+
+    const client_mod = b.createModule(.{
+        .root_source_file = b.path("src/client/main.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const client = b.addExecutable(.{
+        .name = "client",
+        .root_module = client_mod,
+    });
+    client.linkLibrary(libpong);
+    b.installArtifact(client);
+
+    const server_mod = b.createModule(.{
+        .root_source_file = b.path("src/server/main.zig"),
         .target = target,
         .optimize = optimize,
     });
 
     // Main executable
-    const exe = b.addExecutable(.{
+    const server = b.addExecutable(.{
         .name = "ssps",
-        .root_module = exe_mod,
+        .root_module = server_mod,
     });
-    b.installArtifact(exe);
+    server.linkLibrary(libpong);
+    b.installArtifact(server);
 
-    const run_cmd = b.addRunArtifact(exe);
+    const run_cmd = b.addRunArtifact(server);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
         run_cmd.addArgs(args);
@@ -26,18 +52,18 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
+        .root_module = server_mod,
     });
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
 
-    const check_exe = b.addExecutable(.{
+    const check_server = b.addExecutable(.{
         .name = "ssps",
-        .root_module = exe_mod,
+        .root_module = server_mod,
     });
 
     const check_step = b.step("check", "Check the compilation");
-    check_step.dependOn(&check_exe.step);
+    check_step.dependOn(&check_server.step);
 }
