2deca5ecff600cfad40ce3ce3fefac2710cd1d9d
lmoran <112955036+potentiallyme@users.noreply.github.com>
2025-01-29 20:15:34 +0100

    friends system + fixtures

--- srcs/requirements/django/transcendence/settings.py
+++ srcs/requirements/django/transcendence/settings.py
@@ -164,7 +164,7 @@ SIMPLE_JWT = {
     'JTI_CLAIM': 'jti',
 
     'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
-    'SLIDING_TOKEN_LIFETIME': tigcam "removed venv files, reset migrations (not fixed), updated requirements.txt" medelta(days=30),
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=30),
 }
 
 AUTH_USER_MODEL = 'trans.CustomUserTrans'
